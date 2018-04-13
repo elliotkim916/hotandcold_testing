6db8c3259b11aad08021dcff4aaf770c486092c7
Elliot Kim <elliotkim916@gmail.com>
2018-04-13 16:17:50 -0700

    added testing for guess-count component

---
+++ src/components/guess-count.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import GuessCount from './guess-count';
+
+describe('<GuessCount />', () => {
+    it('renders without crashing', () => {
+        shallow(<GuessCount />);
+    });
+
+    it('renders number of guesses', () => {
+        const TEST_GUESSCOUNT = 10;
+        const wrapper = shallow(<GuessCount guessCount={TEST_GUESSCOUNT} />);
+        expect(wrapper.text()).toEqual(`You've made ${TEST_GUESSCOUNT} guesses!`);
+    });
+});
