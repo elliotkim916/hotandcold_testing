d12262d57f4ee733e5bcad9e2ef5f138f830eed0
Elliot Kim <elliotkim916@gmail.com>
2018-04-13 16:05:56 -0700

    create test for feedback component

---
+++ src/components/feedback.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Feedback from './feedback';
+
+describe('<Feedback />', () => {
+    it('Renders without crashing', () => {
+        // shallow function renders Feedback component
+        shallow(<Feedback />);
+    });
+
+    it('Renders test feedback', () => {
+        const TEST_FEEDBACK = "foo";
+        const wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} />);
+        expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+    });
+});
