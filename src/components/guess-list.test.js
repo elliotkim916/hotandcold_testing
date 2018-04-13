b5188908cc65c743edd11d04e5a15e1b5cbb8040
Elliot Kim <elliotkim916@gmail.com>
2018-04-13 16:41:28 -0700

    added testing guess-list component

---
+++ src/components/guess-list.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import GuessList from './guess-list';
+
+describe('<GuessList />', () => {
+   it('Renders without crashing', () => {
+       shallow(<GuessList guesses={[]}/>);
+   });
+
+   it('Renders all guesses', () => {
+       const TEST_GUESSES = [1, 2, 3, 4, 5];
+       const wrapper = shallow(<GuessList guesses={TEST_GUESSES}/>);
+       const listItems = wrapper.find('li');
+       expect(listItems.length).toEqual(TEST_GUESSES.length);
+       TEST_GUESSES.forEach((value, index) => {
+           expect(listItems.at(index).text()).toEqual(value.toString());
+       });
+   });
+});
