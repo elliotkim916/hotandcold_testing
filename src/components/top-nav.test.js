600f342af507a9711d86f4ab122a3be486cdb6a1
Elliot Kim <elliotkim916@gmail.com>
2018-04-17 14:12:13 -0700

    added comments for event handlers

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+// enzyme uses Jest
 
 import TopNav from './top-nav';
 
@@ -12,13 +13,15 @@ describe('<TopNav />', () => {
         const callback = jest.fn();
         const wrapper = shallow(<TopNav onRestartGame={callback} />);
         const link = wrapper.find('.new');
-        link.simulate('click', {preventDefault() {} });
+        link.simulate('click', {preventDefault() {} } );
+        // everything after the click event handler is syntax that enzyme uses
+        // we are passing data through an event
         expect(callback).toHaveBeenCalled();
     });
 
     it('Should call onGenerateAuralUpdate when Hear state of game is clicked', () => {
         const callback = jest.fn();
-        const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback}/>);
+        const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
         const link = wrapper.find('.status-link');
         link.simulate('click', {preventDefault() {} });
         expect(callback).toHaveBeenCalled();
