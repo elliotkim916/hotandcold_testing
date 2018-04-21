41bb3f225997532381ef920ddc9393df44c88970
Elliot Kim <elliotkim916@gmail.com>
2018-04-20 20:42:16 -0700

    testing dispatch to see restartGame and generateAuralUpdate is triggered when user clicks in the DOM

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 // enzyme uses Jest
-
-import TopNav from './top-nav';
+import {TopNav} from './top-nav';
+import {restartGame, generateAuralUpdate} from '../actions';
+// import TopNav from './top-nav';
 
 describe('<TopNav />', () => {
     it('Renders without crashing', () => {
         shallow(<TopNav />);
     });
 
-    it('Should call onRestartGame when new game is clicked', () => {
-        const callback = jest.fn();
-        const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    // it('Should call onRestartGame when new game is clicked', () => {
+    //     const callback = jest.fn();
+    //     const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    //     const link = wrapper.find('.new');
+    //     link.simulate('click', {preventDefault() {} } );
+    //     // everything after the click event handler is syntax that enzyme uses
+    //     // we are passing data through an event
+    //     expect(callback).toHaveBeenCalled();
+    // });
+
+    it('Dispatches restartGame', () => {
+        const dispatch = jest.fn();
+// jest.fn() is used to make a spy, spies are small functions that keep a record of each time they have been called. 
+// They are useful for testing callback functions
+        const wrapper = shallow(<TopNav dispatch={dispatch}/>);
         const link = wrapper.find('.new');
-        link.simulate('click', {preventDefault() {} } );
-        // everything after the click event handler is syntax that enzyme uses
-        // we are passing data through an event
-        expect(callback).toHaveBeenCalled();
+        link.simulate('click');
+        expect(dispatch).toHaveBeenCalledWith(restartGame());
     });
 
-    it('Should call onGenerateAuralUpdate when Hear state of game is clicked', () => {
-        const callback = jest.fn();
-        const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+    // it('Should call onGenerateAuralUpdate when Hear state of game is clicked', () => {
+    //     const callback = jest.fn();
+    //     const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+    //     const link = wrapper.find('.status-link');
+    //     link.simulate('click', {preventDefault() {} });
+    //     expect(callback).toHaveBeenCalled();
+    // });
+
+    it('Dispatches generateAuralUpdate', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(<TopNav dispatch={dispatch}/>);
         const link = wrapper.find('.status-link');
-        link.simulate('click', {preventDefault() {} });
-        expect(callback).toHaveBeenCalled();
+        link.simulate('click');
+        expect(dispatch).toHaveBeenCalledWith(generateAuralUpdate());
     });
 });
